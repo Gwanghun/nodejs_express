b3ea548ed93d87f7b7d27240daac1bfd807157c6
hooeni <hooeni@gmail.com>
2018-07-09 16:24:28 +0900

    생활코딩 node.js express

--- main.js
+++ main.js
@@ -1,148 +1,36 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var fs = require('fs');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var qs = require('querystring');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var indexRouter = require('./routes/index');
+var topicRouter = require('./routes/topic');
+var helmet = require('helmet');
+app.use(helmet()); 
 
-// Route, Routing
-app.get('/', function(request, response) {
+app.use(express.static('public'));		// 정적인 파일 서비스? 파일 직접 불러오기
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+app.get('*', function (request, response, next) {
 	fs.readdir('./data', function(error, filelist){
-		var title = 'Welcome';
-		var description = 'Hello, Node.js';
-		var list = template.list(filelist);
-		var html = template.HTML(title, list,
-			`<h2>${title}</h2>${description}`,
-			`<a href="/create">create</a>`
-		);
-		response.send(html);
+		request.list = filelist;
+		next();
 	});
 });
 
-app.get('/page/:pageId', function(request, response) {
-	fs.readdir('./data', function(error, filelist){
-	  var filteredId = path.parse(request.params.pageId).base;
-	  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-		var title = request.params.pageId;
-		var sanitizedTitle = sanitizeHtml(title);
-		var sanitizedDescription = sanitizeHtml(description, {
-		  allowedTags:['h1']
-		});
-		var list = template.list(filelist);
-		var html = template.HTML(sanitizedTitle, list,
-		  `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-		  ` <a href="/create">create</a>
-			<a href="/update/${sanitizedTitle}">update</a>
-			<form action="/delete_process" method="post">
-			  <input type="hidden" name="id" value="${sanitizedTitle}">
-			  <input type="submit" value="delete">
-			</form>`
-		);
-		response.send(html);
-	  });
-	});
-});
+app.use('/', indexRouter);
+app.use('/topic', topicRouter);
 
-app.get('/create', function(request, response) {
-	fs.readdir('./data', function(error, filelist){
-	  var title = 'WEB - create';
-	  var list = template.list(filelist);
-	  var html = template.HTML(title, list, `
-		<form action="/create_process" method="post">
-		  <p><input type="text" name="title" placeholder="title"></p>
-		  <p>
-			<textarea name="description" placeholder="description"></textarea>
-		  </p>
-		  <p>
-			<input type="submit">
-		  </p>
-		</form>
-	  `, '');
-	  response.send(html);
-	});
+// 불러올 파일이 없을때
+app.use(function(request, response, next) {
+	response.status(404).send('Sorry cant find that!');
 });
+// 에러페이지 처리
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
-app.post('/create_process', function(request, response) {
-	var body = '';
-	request.on('data', function(data){
-		body = body + data;
-	});
-	request.on('end', function(){
-		var post = qs.parse(body);
-		var title = post.title;
-		var description = post.description;
-		fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-		  response.writeHead(302, {Location: `/page/${title}`});
-		  response.end();
-		})
-	});
-});
-
-app.get('/update/:pageId', function(request, response){
-	fs.readdir('./data', function(error, filelist){
-	  var filteredId = path.parse(request.params.pageId).base;
-	  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-		var title = request.params.pageId;
-		var list = template.list(filelist);
-		var html = template.HTML(title, list,
-		  `
-		  <form action="/update_process" method="post">
-			<input type="hidden" name="id" value="${title}">
-			<p><input type="text" name="title" placeholder="title" value="${title}"></p>
-			<p>
-			  <textarea name="description" placeholder="description">${description}</textarea>
-			</p>
-			<p>
-			  <input type="submit">
-			</p>
-		  </form>
-		  `,
-		  `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-		);
-		response.send(html);
-	  });
-	});
-});
-
-app.post('/update_process', function(request, response) {
-	var body = '';
-	request.on('data', function(data){
-		body = body + data;
-	});
-	request.on('end', function(){
-		var post = qs.parse(body);
-		var id = post.id;
-		var title = post.title;
-		var description = post.description;
-		fs.rename(`data/${id}`, `data/${title}`, function(error){
-		  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-			response.redirect(`/page/${title}`);
-		  })
-		});
-	});
-});
-
-app.post('/delete_process', function(request, response) {
-	var body = '';
-	request.on('data', function(data){
-		body = body + data;
-	});
-	request.on('end', function(){
-		var post = qs.parse(body);
-		var id = post.id;
-		var filteredId = path.parse(id).base;
-		fs.unlink(`data/${filteredId}`, function(error){
-		  response.redirect('/');
-		})
-	});
-});
-
-/*
-app.get('/page', function(request, response) {
-
-});
-*/
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 /*
@@ -173,4 +61,9 @@ var app = http.createServer(function(request,response){
     }
 });
 app.listen(3000);
+*/
+/*
+app.get('/page', function(request, response) {
+
+});
 */
